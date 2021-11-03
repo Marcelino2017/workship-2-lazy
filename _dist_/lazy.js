4aacc93523c24ce3b438c97d5fdd6364cef78d9c
Marcelino2017 <Marcelino2017@users.noreply.github.com>
2021-11-03 23:43:35 +0000

    deploy: 6780b66573ea8b0a9d59307bc8bb31ae78a3c1c7

--- _dist_/lazy.js
+++ _dist_/lazy.js
@@ -2,17 +2,23 @@ const isIntersecting = (entry) => {
     return entry.isIntersecting; // devuel true (si esta dentro de la pantalla)
 }
 
-const accion = (entry) => {
-    const nodo = entry.target;
-    console.log('Hola');
+const loadImage = (entry) => {
+    const container = entry.target;
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src;
 
-    
+    imagen.src = url;
+
+    //debugger
+    //console.log(container.nodeName);
+
+
     //des regidtra la imagen
-    observer.unobserve(nodo)
+    observer.unobserve(container)
 };
 
 const observer = new IntersectionObserver((entries) => {
-    entries.filter(isIntersecting).forEach(accion)
+    entries.filter(isIntersecting).forEach(loadImage)
 });
 
 //
